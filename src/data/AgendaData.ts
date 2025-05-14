3880d6ae0206b8195efd4ce63593497e90de7715
Mohamed Asath <asath12882@gmail.com>
2025-05-14 13:42:48 +0000

    upgraded astro

---
+++ src/data/AgendaData.ts
@@ -0,0 +1,88 @@
+export const AgendaData = [
+  {
+    title: "Opening & Speech by Dr. Kalpani Manathunga",
+    time: "1:00 PM",
+    imageURL: "/img/speakers/dinal.png",
+    name: "Dr. Kalpani Manathunga",
+    position: "HOD, SE Department SLIIT",
+  },
+  {
+    title: "Staff Introduction",
+    time: "1:10 PM",
+  },
+  {
+    title: "Speech by Prof. Nuwan Kodagoda",
+    time: "1:15 PM",
+    imageURL: "/img/speakers/dinal.png",
+    name: "Prof. Nuwan Kodagoda",
+    position: "Pro Vice Chancellor, SLIIT",
+  },
+  {
+    title: "Address by Prof. Pradeep Abeygunawardhana",
+    time: "1:30 PM",
+    imageURL: "/img/speakers/dinal.png",
+    name: " Prof. Pradeep Abeygunawardhana",
+    position: "Dean, Faculty Of Computing, SLIIT",
+  },
+  {
+    title: "Entertainment Session",
+    time: "1:40 PM",
+  },
+  /*{
+    title: "Speech by Mr. Ashan Priyadarshana",
+    time: "1:45 PM",
+    imageURL: "/faces/", //face needs to be changed
+    name: "Mr. Ashan Priyadarshana",
+    position: "Tech Lead, Sysco Labs",
+  },*/
+  {
+    title: "Speech by Ms. Madhumini Gunaratne",
+    time: "2:05 PM",
+    imageURL: "/img/speakers/dinal.png",
+    name: "Ms. Madhumini Gunaratne",
+    position: "Research Assistant, Faculty Of Computing, SLIIT",
+  },
+  {
+    title: "Speech by Mr. Ashan Priyadarshana",
+    time: "2:20 PM",
+    imageURL: "/img/speakers/dinal.png",
+    name: "Mr. Ashan Priyadarshana",
+    position: "Tech Lead, Sysco Labs",
+  },
+  {
+    title: "Speech by Mr. Saman Gunawardena",
+    time: "2:40 PM",
+    imageURL: "/img/speakers/dinal.png",
+    name: "Mr. Saman Gunawardena",
+    position: "Senior Manager Industrial Liaison and Placement",
+  },
+  {
+    title: "Speech by Mr. Pasindu Dinal",
+    time: "2:55 PM",
+    imageURL: "/img/speakers/dinal.png",
+    name: "Pasindu Dinal",
+    position: "President, SESC 2024/25",
+  },
+  {
+    title: "Speech by Ms. Subhashini Wijerathna",
+    time: "3:10 PM",
+    imageURL: "/img/speakers/dinal.png",
+    name: "Ms. Subhashini Wijerathna",
+    position:
+      "Associate Manager in Software Engineering, VitalHub Innovations Lab",
+  },
+  {
+    title: "Speech by Ms. Sandamali Vidanagamage",
+    time: "3:10 PM",
+    imageURL: "/img/speakers/dinal.png",
+    name: " Ms. Sandamali Vidanagamage",
+    position: "Manager in Software Engineering, VitalHub Innovations Lab",
+  },
+  {
+    title: "Vote of Thanks",
+    time: "3:40 PM",
+    imageURL: "/img/speakers/dinal.png",
+    name: "Pawan Senpura",
+    position: "Dev Relation, SESC 2024/25",
+  },
+];
