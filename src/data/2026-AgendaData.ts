500e7e3bb0b61aeda9b8c16d98d78c42f0b25d1b
Heshan Thenura Kariyawasam <heshanthenura@protonmail.com>
2026-06-12 00:05:47 +0530

    Revert "chore: change title"
    This reverts commit 0d3e9df02283d93914fb70803580b1333cf601fa.

--- src/data/2026-AgendaData.ts
+++ src/data/2026-AgendaData.ts
@@ -1,10 +1,10 @@
 export const AgendaData = [
   {
-    title: "Welcome Speech",
+    title: "Welcome Speech by the Secretary of Women in FOSS (WIF) for 2026/27",
     time: "9:30 AM",
     imageURL: "/img/speakers/sayuni.jpeg",
     name: "Sayuni Bulathgama",
-    position: "Secretary, Women in FOSS (WIF) 2026/27",
+    position: "Secretary, Women in FOSS",
   },
   {
     title: "Speech by Prof. Nuwan Kodagoda",
